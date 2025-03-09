ff92d722bc0a3e684be9117f567b34e1f325977e
Luis Carlos Saravia <micompufunciona@gmail.com>
2025-03-08 20:30:14 -0600

    0803252029 visualizacion mejorada en historial

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -4,91 +4,176 @@ import '../providers/pdf_history_provider.dart';
 import 'pdf_viewer.dart';
 import '../models/pdf_document.dart';
 import 'package:intl/intl.dart';
+import '../providers/theme_provider.dart';
+import '../utils/theme_data.dart';
 
 class HistoryScreen extends ConsumerStatefulWidget {
-  const HistoryScreen({Key? key}) : super(key: key);
+  const HistoryScreen({super.key});
 
   @override
   _HistoryScreenState createState() => _HistoryScreenState();
 }
 
-class _HistoryScreenState extends ConsumerState<HistoryScreen>
-    with SingleTickerProviderStateMixin {
-  late TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 2, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
+class _HistoryScreenState extends ConsumerState<HistoryScreen> {
+  int _selectedTabIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     final history = ref.watch(pdfHistoryProvider);
-    final abiertos = history.where((pdf) => pdf.status == 'abierto').toList();
-    final guardados = history.where((pdf) => pdf.status == 'guardado').toList();
+    final themeState = ref.watch(themeProvider);
+    final abiertos = history.where((pdf) => pdf.status == 'abierto').toList().reversed.toList();
+    final guardados = history.where((pdf) => pdf.status == 'guardado').toList().reversed.toList();
 
-    print('Abiertos: ${abiertos.map((pdf) => pdf.path).toList()}'); // Agregado para depuración
-    print('Guardados: ${guardados.map((pdf) => pdf.path).toList()}'); // Agregado para depuración
+    print('Abiertos: ${abiertos.map((pdf) => pdf.path).toList()}');
+    print('Guardados: ${guardados.map((pdf) => pdf.path).toList()}');
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Historial'),
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: const [
-            Tab(text: 'Abiertos'),
-            Tab(text: 'Guardados'),
-          ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(70.0), // Ajustar la altura
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _selectedTabIndex = 0;
+                      });
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Abiertos',
+                            style: TextStyle(
+                              fontSize: _selectedTabIndex == 0 ? 20 : 12, // Tamaños de fuente ajustados
+                            ),
+                          ),
+                          if (_selectedTabIndex == 0)
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0), // Espacio aumentado
+                              child: Icon(Icons.check,
+                                  color: subThemes[themeState.selectedSubTheme]!,
+                                  size: 24.0), // Icono más grande
+                            ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _selectedTabIndex = 1;
+                      });
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Guardados',
+                            style: TextStyle(
+                              fontSize: _selectedTabIndex == 1 ? 20 : 12, // Tamaños de fuente ajustados
+                            ),
+                          ),
+                          if (_selectedTabIndex == 1)
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0), // Espacio aumentado
+                              child: Icon(Icons.check,
+                                  color: subThemes[themeState.selectedSubTheme]!,
+                                  size: 24.0), // Icono más grande
+                            ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Divider(height: 1.0, thickness: 1.0), // Línea divisoria debajo de los títulos
+            ],
+          ),
         ),
       ),
-      body: TabBarView(
-        controller: _tabController,
+      body: Column(
         children: [
-          _buildPdfList(abiertos),
-          _buildPdfList(guardados),
+          Container(height: 16),
+          Expanded(
+            child: IndexedStack(
+              index: _selectedTabIndex,
+              children: [
+                _buildPdfList(abiertos),
+                _buildPdfList(guardados),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 
   Widget _buildPdfList(List<PdfDocument> pdfList) {
-    print('history_screen: Reconstruyendo lista con ${pdfList.length} elementos'); // Registro
+    print('history_screen: Reconstruyendo lista con ${pdfList.length} elementos');
 
     return ListView.builder(
       itemCount: pdfList.length,
       itemBuilder: (context, index) {
         final pdf = pdfList[index];
-        return ListTile(
-          title: Text(pdf.name, style: const TextStyle(fontSize: 18)),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Estado: ${pdf.status}',
-                  style: const TextStyle(fontSize: 16)),
-              Text('Ruta: ${pdf.path}', style: const TextStyle(fontSize: 16)),
-              if (pdf.originalPath.isNotEmpty)
-                Text('Original: ${pdf.originalPath}',
-                    style: const TextStyle(fontSize: 16)),
-              Text(DateFormat('yyyy-MM-dd HH:mm').format(pdf.date),
-                  style: const TextStyle(fontSize: 16)),
-            ],
-          ),
-          trailing: IconButton(
-            icon: const Icon(Icons.open_in_new),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => PdfViewerPage(pdfDocument: pdf)),
-              );
-            },
-          ),
+        return Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(DateFormat('yyyy-MM-dd HH:mm').format(pdf.date)),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(pdf.name, style: const TextStyle(fontSize: 16)),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: IconButton(
+                    icon: const Icon(Icons.open_in_new),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => PdfViewerPage(pdfDocument: pdf)),
+                      );
+                    },
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: ExpansionTile(
+                    title: const Icon(Icons.folder),
+                    children: [
+                      ListTile(
+                        title: Text('Ruta: ${pdf.path}', style: const TextStyle(fontSize: 16)),
+                      ),
+                      if (pdf.originalPath.isNotEmpty)
+                        ListTile(
+                          title: Text('Original: ${pdf.originalPath}',
+                              style: const TextStyle(fontSize: 16)),
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Divider(),
+          ],
         );
       },
     );
